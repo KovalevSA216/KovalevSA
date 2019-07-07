f90a23bb889a98f86ab0c34035c80cd9771a5b90
khorne2106@mail.ru <khorne2106@mail.ru>
2019-07-07 15:48:52 +1000

    Добавьте файлы проекта.

---
+++ Laba№2/2.cpp
@@ -0,0 +1,30 @@
+#include "conio.h"
+#include "locale.h"
+#include "iostream"
+
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	double xnach = -1.0, xkon = 1.0, dx = 0.01, eps = 0.001;
+	printf("Табулирование функции в виде таблицы:\n");
+	printf("x\t\tF(x)\t\tКоличество\n");
+	while (xnach <= xkon)
+	{
+		int Kolvo = 1, a = 2, b = 2, c = 3, d = 5;
+		double Fx = (double)a*powf(xnach, (double)b) / (double)c;
+		while (powf(-1, b)*(double)a*powf(xnach, (double)b) / (double)c >= eps)
+		{
+			Fx = Fx + powf(-1, b)*(double)a*powf(xnach, (double)b) / (double)c;
+			Kolvo++;
+			a++;
+			b = b + 2;
+			c = c + d;
+			d = d + 2;
+		}
+		printf("%lf\t%lf\t%d\n", xnach, Fx, Kolvo);
+		xnach = xnach + dx;
+	}
+	getch();
+}
